a52324f4342857bb9fb5e0a7dac7f23d63cc1cb0
Jayden Wool <52765228+jwoolnt@users.noreply.github.com>
2025-07-19 06:25:31 +0000

    update compile.py

--- src/compile.py
+++ src/compile.py
@@ -1,4 +1,4 @@
-from os import path, mkdir, remove
+from os import path, mkdir, remove, getcwd, chdir
 from shutil import rmtree, copyfile
 
 from intelqsdk.cbindings import compileProgram, loadSdk
@@ -7,22 +7,30 @@ from intelqsdk.cbindings import compileProgram, loadSdk
 COMPILER_PATH = "/opt/intel/quantum-sdk/latest/intel-quantum-compiler"
 SRC_PATH = path.dirname(path.realpath(__file__))
 OUTPUT_FOLDER = "qbuild"
-VISUALIZATION_FOLDER = path.join(SRC_PATH, "Visualization")
+VISUALIZATION_FOLDER = "Visualization"
 VISUALIZATION_OPTIONS = {"console", "tex", "json"}
 
 
 def compileAndLoad(
 		sdk_name: str,
 		/,
-		output_folder = OUTPUT_FOLDER,
+		output_folder: str = OUTPUT_FOLDER,
+		visualization_folder: str = VISUALIZATION_FOLDER,
 		*,
+		src: bool = True,
 		clear: bool = True,
 		copy_compile: bool = True,
-		visualization: str | None = None
+		visualization: str | None = None # TODO: Support multiple visualizations at once
 	):
 	file_name = f"{sdk_name}.cpp"
-	file_path = path.join(SRC_PATH, file_name)
-	output_path = path.join(SRC_PATH, output_folder)
+	file_path = file_name
+	output_path = output_folder
+	visualization_path = visualization_folder
+	working_path = getcwd()
+	if src:
+		file_path = path.join(SRC_PATH, file_name)
+		output_path = path.join(SRC_PATH, output_folder)
+		visualization_path = path.join(SRC_PATH, visualization_folder)
 
 	def if_exists(check_path: str, function, inverse: bool = False, *args, **kwargs):
 		if path.exists(check_path) ^ inverse:
@@ -30,7 +38,7 @@ def compileAndLoad(
 
 	if clear:
 		if_exists(output_path, rmtree, ignore_errors=True)
-		if_exists(VISUALIZATION_FOLDER, rmtree, ignore_errors=True)
+		if_exists(visualization_path, rmtree, ignore_errors=True)
 
 	if_exists(output_path, mkdir, True)
 	if copy_compile:
@@ -44,9 +52,9 @@ def compileAndLoad(
 	flags.append("-s")
 	flags = " ".join(flags)
 
+	if src: chdir(SRC_PATH)
 	compileProgram(COMPILER_PATH, file_path, flags, sdk_name)
-
-	# TODO: use "local" Visualization folder?
+	if src: chdir(working_path)
 
 	if copy_compile:
 		remove(file_path)
